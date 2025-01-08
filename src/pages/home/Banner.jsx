c5512b6058e73ddcb1704cc53f86f3bc90ba9c9d
gitmahmudulh <connect.mahmudulh@gmail.com>
2025-01-08 08:10:14 +0600

    added hero banner

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import bannerImage1 from '../../assets/home/01.jpg';
+import bannerImage2 from '../../assets/home/02.jpg';
+import bannerImage3 from '../../assets/home/03.jpg';
 
 const Banner = () => {
     return (
         <div>
             <Carousel>
                 <div>
-                    <img src="assets/1.jpeg" />
-                    <p className="legend">Legend 1</p>
+                    <img src={bannerImage1} />
                 </div>
                 <div>
-                    <img src="assets/2.jpeg" />
-                    <p className="legend">Legend 2</p>
+                    <img src={bannerImage2} />
                 </div>
                 <div>
-                    <img src="assets/3.jpeg" />
-                    <p className="legend">Legend 3</p>
+                    <img src={bannerImage3} />
                 </div>
             </Carousel>
         </div>
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed top-0 z-10 bg-black/60 text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
